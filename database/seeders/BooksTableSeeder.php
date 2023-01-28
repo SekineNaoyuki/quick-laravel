b5b7a90b1dc19c78791c4aaf5d8269f9e8bc5781
SekineNaoyuki <thierry.daniel.henry0302@gmail.com>
2023-01-28 22:18:07 +0900

    seed

---
+++ database/seeders/BooksTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BooksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::insert('INSERT INTO books (
+            isbn,
+            title,
+            price,
+            publisher,
+            published
+        ) VALUES(
+            "978-4-8222-5399-8",
+            "Visual C# 超入門",
+            2000,
+            "ジャパンIT",
+            "2022-08-22"
+        )');
+    }
+}
