d92e1148196a76fb4155ccf1b616d015b6c13fa5
SekineNaoyuki <thierry.daniel.henry0302@gmail.com>
2023-01-28 21:45:46 +0900

    form

--- resources/views/ctrl/form.blade.php
+++ resources/views/ctrl/form.blade.php
@@ -1,11 +1,14 @@
 @extends('layouts.base')
 @section('title','フォームの基本')
 @section('main')
-<form method="POST"action="/ctrl/result">
+@if (session('alert'))
+    <div class="alert">{{ session('alert') }}</div>
+@endif
+<form method="POST" action="/ctrl/result">
     @csrf
     <label id="name">名前：</label>
-    <input id="name"name="name"type="text"value=""/>
-    <input type="submit"value="送信"/>
+    <input id="name" name="name" type="text" value="{{ old('name', '') }}" />
+    <input type="submit" value="送信"/>
     <p>{{$result}}</p>
 </form>
 @endsection
